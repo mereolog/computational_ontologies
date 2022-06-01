890426d8b7a0f19b8bcf6e2b08e497b2f8ea3e30
mereolog <pawel.garbacz@makolab.com>
2022-06-01 09:25:43 +0200

    first run of consistency checks for BFO subtheories
    Signed-off-by: mereolog <pawel.garbacz@makolab.com>

--- python/computable_ontologies/subtheories_checker.py
+++ python/computable_ontologies/subtheories_checker.py
@@ -10,7 +10,7 @@ def __get_all_axioms_from_text(text: str):
     return axioms
 
 
-def __identify_axioms(axioms: list) -> list:
+def __identify_axioms_by_first_variable_used(axioms: list) -> list:
     identified_axioms = list()
     variable_pattern = re.compile(r'X\d+')
     atomic_count = 0
@@ -31,34 +31,35 @@ def __check_subaxioms(axioms: list, depth: int):
         sub_axioms = list(sub_axioms_tuple)
         sub_axioms.sort()
         axiom_text = '\n'.join(sub_axioms)
-        axioms_to_be_checked_file_id = str(hashlib.md5(axiom_text.encode()).hexdigest())
-        axioms_to_be_checked_file_name = 'in/' + axioms_to_be_checked_file_id + '.in'
-        axioms_out_text_file = open(axioms_to_be_checked_file_name, 'w')
-        axioms_out_text_file.write(axiom_text)
-        axioms_out_text_file.close()
+        axioms_file_content_hash = str(hashlib.md5(axiom_text.encode()).hexdigest())
+        axioms_file_name = 'in/' + axioms_file_content_hash + '.in'
+        axioms_file = open(axioms_file_name, 'w')
+        axioms_file.write(axiom_text)
+        axioms_file.close()
         
-        cmd = \
+        cmd_to_run_vampire = \
             'Resources/vampire_rel_master_6172 ' + \
             ' --mode casc_sat ' + \
-            axioms_to_be_checked_file_name + \
-            ' > out/' + axioms_to_be_checked_file_id + '.out'
+            axioms_file_name + \
+            ' > out/' + axioms_file_content_hash + '.out'
         
-        result = os.system(cmd)  # returns the exit status
-        if result == 0:
-            print(axioms_to_be_checked_file_id)
+        vampire_result = os.system(cmd_to_run_vampire)  # returns the exit status
+        vampire_has_decided = vampire_result == 0
+        if vampire_has_decided:
+            print('Vampire decided about ' + axioms_file_content_hash)
       
             
 if __name__ == '__main__':
     axioms_in_text_file = open('Resources/bfo.tptp')
     axioms_text = axioms_in_text_file.read()
     axioms_in_text = __get_all_axioms_from_text(text=axioms_text)
-    identified_axioms_in_text = __identify_axioms(axioms=axioms_in_text)
+    identified_axioms_in_text = __identify_axioms_by_first_variable_used(axioms=axioms_in_text)
     identified_axioms_in_text.sort()
     __check_subaxioms(axioms=identified_axioms_in_text, depth=1)
 else:
     axioms_in_text_file = open('Resources/bfo.tptp')
     axioms_text = axioms_in_text_file.read()
     axioms_in_text = __get_all_axioms_from_text(text=axioms_text)
-    identified_axioms_in_text = __identify_axioms(axioms=axioms_in_text)
+    identified_axioms_in_text = __identify_axioms_by_first_variable_used(axioms=axioms_in_text)
     identified_axioms_in_text.sort()
     __check_subaxioms(axioms=identified_axioms_in_text, depth=1)
