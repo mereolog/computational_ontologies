73b633eeaf38be95af87b4f723e508ebc361950e
mereolog <pawel.garbacz@makolab.com>
2022-06-07 16:15:22 +0200

    colore ontologies resolvable
    Signed-off-by: mereolog <pawel.garbacz@makolab.com>

--- python/computable_ontologies/colore_deresolver.py
+++ python/computable_ontologies/colore_deresolver.py
@@ -8,12 +8,11 @@ for path, subdirs, file_names in os.walk(root):
     for file_name in file_names:
         file_path = os.path.join(path, file_name)
         file = open(file_path, 'r')
-        if file_name == '.DS_Store':
-            continue
-        file_content = file.read()
-        file.close()
-        new_file_content = iri_regex.sub(repl='\1 ala_ma_kota', string=file_content)
-        file = open(file_path, 'w')
-        file.write(new_file_content)
-        file.close()
+        if file_name.endswith('clif'):
+            file_content = file.read()
+            file.close()
+            file = open(file_path, 'w')
+            new_file_content = file_content.replace('http://colore.oor.net/','https://raw.githubusercontent.com/mereolog/computational_ontologies/main/TUpper/')
+            file.write(new_file_content)
+            file.close()
         
